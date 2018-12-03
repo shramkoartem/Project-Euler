a1e5f75c58b744bfe95b587110e831b38db4c200
Artem Shramko <art.shramko@gmail.com>
2018-12-03 23:09:11 +0100

    Update 28.py

--- 28.py
+++ 28.py
@@ -1,17 +1,23 @@
-n = [i for i in range(1,1002)]
+1001*1001
+1002001
+
+n = [i for i in range(1,1002002)]
 
 res = []
+count=0
 step = 0
-while len(res)<1001:
+while len(res)<1002001:
     pos = 0
     try:
-        for j in range(2,100,2):
+        for j in range(2,20000,2):
             for i in range(1,5):
                 #step += j
                 res.append(n[pos+step]) 
                 step += j
             pos = res[-1]-1
             step = j
+            count += 1
     except IndexError:
         break
-res
+        
+sum(res)
