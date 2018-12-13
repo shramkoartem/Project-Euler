42b87565b4aaf211fa1c1360b7a7d984b96e731a
Artem Shramko <art.shramko@gmail.com>
2018-12-14 00:19:17 +0100

    Update 59.py

--- 59.py
+++ 59.py
@@ -4,5 +4,17 @@ for line in f:
 s = str(s)[:-1]
 s = s.split(",")
 s = [int(i) for i in s]
-x = [chr(i) for i in s]
 
+
+r = []
+for a in range(97,123):
+    for b in range(97,123):
+        for c in range(97,123):
+            row = []
+            for i in range(0,len(s)-2,3):
+                q = [chr(s[i]^a), chr(s[i+1]^b), chr(s[i+2]^c)]
+                for j in q:
+                    row.append(j)
+            r.append(row)
+
+ 
