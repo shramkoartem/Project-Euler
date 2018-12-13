fd9e01424d61e4444ee0b021b782285eebb5a807
Artem Shramko <art.shramko@gmail.com>
2018-12-14 00:57:54 +0100

    Update 59.py

--- 59.py
+++ 59.py
@@ -6,15 +6,30 @@ s = s.split(",")
 s = [int(i) for i in s]
 
 
-r = []
+p = []
 for a in range(97,123):
     for b in range(97,123):
         for c in range(97,123):
             row = []
-            for i in range(0,len(s)-2,3):
+            for i in range(0,len(s)-1,3):
                 q = [chr(s[i]^a), chr(s[i+1]^b), chr(s[i+2]^c)]
                 for j in q:
                     row.append(j)
-            r.append(row)
-
+            if bool(re.match("[ a-z-(]", "".join(row))):
+                print(a,b,c)
+                print(row[:10])
+                p.append(row)
  
+#103 111 100               
+#['(', 'T', 'h', 'e', ' ', 'G', 'o', 's', 'p', 'e']
+
+a = 103
+b = 111
+c = 100
+row = []        
+for i in range(0,len(s)-1,3):
+    q = [s[i]^a, s[i+1]^b, s[i+2]^c]
+    for j in q:
+        row.append(j)
+
+sum(row)+ord(46)
