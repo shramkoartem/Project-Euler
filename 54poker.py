4167e877fbf527eb40d14bfbbd6c1934ffb9fd42
Artem Shramko <art.shramko@gmail.com>
2018-12-16 23:39:08 +0100

    Update 54poker.py

--- 54poker.py
+++ 54poker.py
@@ -34,3 +34,90 @@ four = 8
 straight_flush = 9
 royal_flush = 10
 
+class Hand():
+    
+    def f_high_card(self,hand):
+        m = 0
+        for card in hand:
+            if card[0] > m:
+                m = card[0]
+        return m
+    
+    def __init__(self, hand):
+        self.hand = hand
+        self.high_card = self.f_high_card(hand)
+        self.score = 1
+    
+    def f_same_kind(self):
+        vals = [i[0] for i in self.hand]
+        v = set(vals)
+
+        cards = []
+        for i in v:
+                c = 0
+                for j in vals:
+                    if j == i:
+                        c += 1
+                cards.append(c)
+        if 4 in cards:
+            return four
+        if 3 in cards:
+            if 2 in cards:
+                return full_house
+            else:
+                return three
+        if 2 in cards:
+            if len(set(cards)) < len(cards):
+                return two_pairs
+            else: return pair
+        return high_card
+    
+    def f_straight(self):
+        vals = [i[0] for i in self.hand]
+        suit = [i[1] for i in self.hand]
+        vals.sort()
+        if len(set(vals)) == len(vals):
+            ans = []
+            for i in range(0,4):
+                if vals[i]+1 != vals[i+1]:
+                    return 0
+            if len(set(suit)) == 1:
+                if vals[0] == 11:
+                    return royal_flush
+                else: return straight_flush
+            else:
+                return straight
+        else: return 0
+    
+    def f_flush(self):
+        suit = [i[1] for i in self.hand]
+        if len(set(suit)) == 1:
+            return flush
+        else: return 0
+        
+    def evaluate(self):
+        res = []
+        a = self.f_same_kind()
+        b = self.f_straight()
+        c = self.f_flush()
+        
+        res.append(a)
+        res.append(b)
+        res.append(c)
+        self.score = max(res)
+    
+    
+count = 0
+for row in cards:
+    p1 = Hand(row[0])
+    p2 = Hand(row[1])
+    p1.evaluate()
+    p2.evaluate()
+    if p1.score == p2.score:
+        if p1.high_card > p2.high_card:
+            count += 1
+            continue
+    if p1.score > p2.score:
+        count += 1
+    
+print(count)
