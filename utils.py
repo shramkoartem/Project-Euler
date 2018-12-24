9fc4acc171195ea08f00363c18db4882452729d8
Artem Shramko <art.shramko@gmail.com>
2018-12-24 11:15:50 +0100

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,14 @@
+def prime(n):
+    if n == 2:
+        return True
+    
+    if n%2 == 0:
+        return False
+    
+    for i in range(3, int(n**0.5) + 1, 2):
+        if n%i == 0:
+            return False
+    return True
+    
+    
+primes = [i for i in range(1,100001) if prime(i)]
