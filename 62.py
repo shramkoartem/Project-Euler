014f3b315f6b44ff228de1be5ec6797a55b28c5c
Artem Shramko <art.shramko@gmail.com>
2018-12-25 13:49:43 +0200

    Create 62.py

---
+++ 62.py
@@ -0,0 +1,27 @@
+cubes = [i**3 for i in range(1000,10000)]
+
+def digits(n):
+    ans = str(n)
+    ans = int("".join(sorted(ans)))
+    return ans
+    
+n = [[i,digits(i)] for i in cubes]
+
+k = [i[1] for i in n]
+
+m = list(set(k))
+
+res = []
+for j in m:
+    a = []
+    c = 0
+    for h in n:
+        
+        if j == h[1]:
+            c += 1
+            a.append(h[0])
+    res.append([a,c])
+    
+for i in res:
+    if i[1] == 5:
+        print(i[0][0])
