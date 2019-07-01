3b51ae453c942ba7b42689fd28f80b0a7017a78e
Artem Shramko <art.shramko@gmail.com>
2019-07-01 21:44:29 +0200

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,20 +1,9 @@
-#Correct solution
+a, b = 1, 2
+s = 0
+while b < 4000000:
+    if b%2==0:
+        s += b
+    a, b = b, a+b
 
-A = [0]
-a, b = 0, 1
-for i in A:
-        a, b = b, a + b
-        A.append(b)
-        y = len(A)-1
-        if A[y]>4000000:
-            break
-        else:
-            continue
+print(s)
 
-del(A[-1])
-
-b = []
-for i in A:
-    if i%2==0:
-      b.append(i)
-sum(b)
