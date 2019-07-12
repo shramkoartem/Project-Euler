44bfd651cae7205b8b0833a8d7b12819163fb5d0
Artem Shramko <art.shramko@gmail.com>
2019-07-12 12:05:45 +0200

    Create MultiplesOf3and5.java

---
+++ MultiplesOf3and5.java
@@ -0,0 +1,12 @@
+public class MultiplesOf3and5 {
+
+    public static void main(String[] args){
+        int s = 0;
+        for(int i = 3; i < 1000; i++){
+            if(i%3==0 || i%5==0){
+                s += i;
+            }
+        }
+        System.out.println(s);
+    }
+}
